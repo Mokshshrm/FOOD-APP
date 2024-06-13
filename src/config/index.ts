1133fc17b0c0b8cb9a6d59496824b24f348b813a
Moksh <mokshsharma1110@gmail.com>
2024-06-13 13:32:08 +0530

    included .env file

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+
+// MongoDB details :
+const PASSWORD = process.env.PASSWORD
+const USER_NAME = process.env.USER_NAME
+const DB_NAME = process.env.DB_NAME
+export const CONNECTION_STR = process.env.CONNECTION_STR
+export const APP_SECERET = process.env.APP_SECERET
+export const PORT = process.env.PORT
+export const AccountSID = process.env.AccountSID
+export const authToken = process.env.authToken
+export const YOURTWILLIONUMBER = process.env.YOURTWILLIONUMBER
